c9d9a3bb5e00ad7ede1d63b7619b441606075494
nrmohanty <142716031+nrmohanty@users.noreply.github.com>
2023-09-22 20:00:13 +0530

    Add files via upload

--- week5/day2/CreateContact.java
+++ week5/day2/CreateContact.java
@@ -7,19 +7,24 @@ import org.testng.annotations.Test;
 public class CreateContact extends BaseClass {
 	
 	@Test
-	public  void CreateContactTestCase() {
+	public  void CreateContactTestCase() throws InterruptedException {
 				
 		// Click on contacts Button
-		driver.findElement(By.xpath("//a[contains(text(),'Contacts')]"));
+		driver.findElement(By.xpath("(//a[contains(text(),'Contacts')])[1]")).click();
 		
 		//Click on Create Contact
 		driver.findElement(By.xpath("//a[contains(text(),'Create')]")).click();
 		
+		Thread.sleep(3000);
+		
 		//Enter FirstName Field Using id Locator
-		driver.findElement(By.id("firstNameField")).sendKeys("Nihar");
+		driver.findElement(By.xpath("//input[@id='firstNameField']")).sendKeys("Nihar");
 		
+		//Enter Lastname field Using id locator
+		driver.findElement(By.xpath("//input[@id='lastNameField']")).sendKeys("Mohanty");
 		
-		
+		//Click on the Create Contact button
+		driver.findElement(By.xpath("//input[@name='submitButton']")).click();
 		
 	}
 
